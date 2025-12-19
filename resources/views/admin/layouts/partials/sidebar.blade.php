2af3399963b6c1f0470f57db6472c13a11189a42
Gusti Bagus Wahyu Saputra <77344072+gusbgs@users.noreply.github.com>
2025-12-19 09:20:51 +0800

    Update sidebar.blade.php

--- resources/views/admin/layouts/partials/sidebar.blade.php
+++ resources/views/admin/layouts/partials/sidebar.blade.php
@@ -128,10 +128,10 @@
             <span class="menu-label">Data Jalan &amp; Aksesbilitas</span>
         </a>
 
-        <a class="menu-item {{ nav_active('admin.skoring.*') }}" href="{{ route('admin.skoring.index') }}">
+        <!-- <a class="menu-item {{ nav_active('admin.skoring.*') }}" href="{{ route('admin.skoring.index') }}">
             <span class="menu-icon"><i class="ri-slideshow-2-line" aria-hidden="true"></i></span>
             <span class="menu-label">Variabel Skoring &amp; Bobot</span>
-        </a>
+        </a> -->
         <a class="menu-item {{ nav_active('admin.kategori-permohonan.*') }}" href="{{ route('admin.kategori-permohonan.index') }}">
             <span class="menu-icon"><i class="ri-file-list-3-line" aria-hidden="true"></i></span>
             <span class="menu-label">Kategori Permohonan</span>
