37819903577c8e20e1d0192ecec78408ade95e52
Gusti Bagus Wahyu Saputra <77344072+gusbgs@users.noreply.github.com>
2026-01-25 16:41:54 +0800

    Hide data infrastruktur (by plano)

--- resources/views/landing/layout/sidebar.blade.php
+++ resources/views/landing/layout/sidebar.blade.php
@@ -112,11 +112,11 @@
             <span class="menu-label">Data Pelanggan</span>
         </a> -->
 
-            <a class="menu-item {{ nav_active('admin.data-infrastruktur.*', 'admin.infrastruktur.*', 'admin.gardu.*', 'admin.pembangkit.*') }}"
+            {{-- <a class="menu-item {{ nav_active('admin.data-infrastruktur.*', 'admin.infrastruktur.*', 'admin.gardu.*', 'admin.pembangkit.*') }}"
                 href="{{ route('admin.data-infrastruktur.index') }}">
                 <span class="menu-icon"><i class="ri-plug-line"></i></span>
                 <span class="menu-label">Data Infrastruktur</span>
-            </a>
+            </a> --}}
 
             <a class="menu-item {{ nav_active('admin.jalan.*') }}" href="{{ route('admin.jalan.index') }}">
                 <span class="menu-icon"><i class="ri-road-map-line" aria-hidden="true"></i></span>
