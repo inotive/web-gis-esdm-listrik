0917a62bae13c501eb6098a4f2c4587dc870e079
siponse <fahrul@nongki.party>
2026-02-08 00:08:57 +0800

    feat: Implement duplicate file name handling by appending a counter to avoid overwrites.

--- app/Helpers/UploadFile.php
+++ app/Helpers/UploadFile.php
@@ -4,31 +4,36 @@ namespace App\Helpers;
 
 use File;
 
-trait UploadFile {
+trait UploadFile
+{
 
     function storeFile($file, $path)
-	{
-        if ( ! File::exists(storage_path('app/public/' . $path)) ) {
+    {
+        if (! File::exists(storage_path('app/public/' . $path))) {
             File::makeDirectory(storage_path('app/public/' . $path), 0755, true, true);
         }
 
         $filenameWithExt = $file->getClientOriginalName();
-        //Get just filename
         $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
-        // Get just ext
         $extension = $file->getClientOriginalExtension();
-        // Filename to store
-        $fileNameToStore = uniqid() . time().'.'.$extension;
-        // Upload Image
-        $path = $file->storeAs($path, $fileNameToStore, 'public');
+
+        $fileNameToStore = $filename . '.' . $extension;
+        $counter = 1;
+
+        while (File::exists(storage_path('app/public/' . $path . '/' . $fileNameToStore))) {
+            $fileNameToStore = $filename . ' (' . $counter . ').' . $extension;
+            $counter++;
+        }
+
+        $file->storeAs($path, $fileNameToStore, 'public');
 
         return $fileNameToStore;
     }
 
     public function deleteFile($file, $path)
     {
-        if ( File::exists(storage_path('app/public/'.$path.'/'.$file)) ){
-            return File::delete(storage_path('app/public/'.$path.'/'.$file));
+        if (File::exists(storage_path('app/public/' . $path . '/' . $file))) {
+            return File::delete(storage_path('app/public/' . $path . '/' . $file));
         }
 
         return false;
